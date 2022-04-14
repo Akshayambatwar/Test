1e97b708c55d9e1b39abb2cb3fd0ec52a1b1151b
Nilshree123 <91187870+Nilshree123@users.noreply.github.com>
2022-04-14 12:39:12 +0530

    first commit

---
+++ src/Grid.js
@@ -0,0 +1,24 @@
+import React, { useRef } from 'react'
+import './App.css';
+
+function Grid() {
+  
+  return (
+    <div className="Grid-Container">
+
+      <div className="gridItem"></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+      <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+      <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+      <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+      <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+      <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+      <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+      <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+      <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+      <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Grid
